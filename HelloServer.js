4e374e45e633435399973ccced3a75bbe25b0b6e
Ling Lu <LingLu@Lings-MacBook-Pro.local>
2014-11-14 11:49:06 -0800

    Sends index.html to client with call back

--- HelloServer.js
+++ HelloServer.js
@@ -1,25 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 http.createServer(function (req, res) {
-	debugger;	
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('<h1> Hello World</h1>\n');
-  res.write('<h1>Hello Counting Loop:</h1>');
-  res.write("<br>");
-   var count;
-  res.write("Counts from 0 to 10" + "<br/>");
-  for(count = 0; count <= 10; count ++){
-  	res.write("Current Count:" + count);
-  	res.write("<br />");
-  }
-  res.write("Count Stop!");
-  res.write("<br />");
-  res.write("<br />");
-  res.write ("Number from 10 to 0" + "<br/>");
-  for(count = 10; count >= 0; count --){
-  	res.write("Number:" + count);
-  	res.write("<br />");
-  } 
-  	res.write("Count Stop!");
-  res.end('<h1>Yahoo! </h1><a href="http://yahoo.com"> homepage</a>\n');
+	debugger;	// TODO take me out
+  fs.readFile("index.html", {encoding : "utf8"}, function (err, data) {
+    if (err) throw err;
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end("Finished my call back");
+  });
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
