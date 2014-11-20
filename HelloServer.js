f1ab46b6f7ea1bfb6428efe7ff193b405e8f0304
Ling Lu <LingLu@Lings-MacBook-Pro.local>
2014-11-20 11:03:52 -0800

    nested file call back

--- HelloServer.js
+++ HelloServer.js
@@ -3,10 +3,15 @@ var fs = require('fs');
 http.createServer(function (req, res) {
 	debugger;	// TODO take me out
   fs.readFile("index.html", {encoding : "utf8"}, function (err, data) {
-    if (err) throw err;
     res.writeHead(200, {'Content-Type': 'text/html'});
+    if (err) throw err;
     res.write(data);
-    res.end("Finished my call back");
+    fs.readFile("body.html", function (err, data) {
+      res.write(data);
+      res.end("Finished my call back");
+      console.log("File 2 call back finished");
+    });
+    console.log("This is end");
   });
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
