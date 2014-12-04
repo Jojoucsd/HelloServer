c7c880150453e2d7e766fa586cb668b232ea5a5b
Ling Lu <jojoucsd@gmail.com>
2014-12-04 12:22:24 -0800

    added mongoose

--- HelloServer.js
+++ HelloServer.js
@@ -1,5 +1,18 @@
 var http = require('http');
+var mongoose = require('mongoose');
 var fs = require('fs');
+
+// connecting to mongodb
+mongoose.connect ('mongodb://localhost/my_database');
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+  if (err) return console.error(err);
+  console.log('meow');
+});
+
+// making http server
 http.createServer(function (req, res) {
 	debugger;	// TODO take me out
   fs.readFile("index.html", {encoding : "utf8"}, function (err, data) {
@@ -8,7 +21,15 @@ http.createServer(function (req, res) {
     res.write(data);
     fs.readFile("body.html", function (err, data) {
       res.write(data);
-      res.end("Finished my call back");
+      // Find a Cat by name.
+      Cat.findOne({ name: 'Zildjian'}, function(err, found){
+        if (err) return console.error(err);
+        console.log('Zildjian');
+        if (found) {
+          res.write("<h2>" +found.name+ "</h2>");
+        }
+        res.end("Finished my call back");
+      });
       console.log("File 2 call back finished");
     });
     console.log("This is end");
